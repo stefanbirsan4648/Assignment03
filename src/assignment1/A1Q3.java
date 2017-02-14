523905f65bfcde3aa012eaafa735106cb2f3ed59
birss0828 <birss0828@HRH-IC0027339.wrdsb.ca>
2017-02-14 09:19:33 -0500

    Working on Questions 4 & 5

--- src/assignment1/A1Q3.java
+++ src/assignment1/A1Q3.java
@@ -7,6 +7,8 @@ package assignment1;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /**
  *
@@ -29,7 +31,89 @@ public class A1Q3 {
         
         Robot romo = new Robot (kitchener,3,0,Direction.EAST); 
         
-        //Creating walls for the Robot
+        //Creating Thing for Robot
+        
+         new Thing(kitchener,3,1);
+         
+         //Creating Walls for robot
+         
+          new Wall(kitchener,3,2,Direction.WEST);  
+          new Wall(kitchener,3,2,Direction.NORTH); 
+          new Wall(kitchener,2,3,Direction.WEST);
+          new Wall(kitchener,1,3,Direction.WEST); 
+          new Wall(kitchener,1,3,Direction.NORTH); 
+          new Wall(kitchener,1,3,Direction.EAST);
+          new Wall(kitchener,2,4,Direction.NORTH);
+          new Wall(kitchener,2,4,Direction.EAST);  
+          new Wall(kitchener,3,4,Direction.EAST);
+          
+          //Directing The Robot Where To Go
+           romo.move();
+           romo.pickThing();
+           romo.turnLeft();
+           romo.move();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.move();
+           romo.turnLeft();
+           romo.move();
+           romo.move();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.move();
+           romo.putThing();
+           romo.move();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.move();
+           romo.turnLeft();
+           romo.move();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.turnLeft();
+           romo.move();
+           romo.move();
+           romo.turnLeft();
+           
+           
+             
+           
+            
+           
+            
+           
+           
+           
+                 
+           
+           
+           
+           
+              
+           
+               
+           
+           
+           
+              
+           
+           
+           
+           
+           
+           
+          
+          
+          
+          
+          
+          
+          
+         
+         
         
         
         
---
+++ src/assignment1/A1Q4.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *
+ * @author birss0828
+ */
+public class A1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+    }
+}
