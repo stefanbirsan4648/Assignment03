4e947377d869fc5bdcd729a23cb00c27f35117d8
birss0828 <birss0828@HRH-IC0027339.wrdsb.ca>
2017-02-28 10:07:39 -0500

    Work at home on Assignment 3 New Changes

--- src/Assignment3/A3Q3.java
+++ src/Assignment3/A3Q3.java
@@ -4,12 +4,7 @@
  */
 package Assignment3;
 
-import becker.robots.City;
-import becker.robots.Direction;
-import becker.robots.Robot;
-import becker.robots.RobotSE;
-import becker.robots.Thing;
-import becker.robots.Wall;
+import becker.robots.*;
 
 /**
  *
@@ -22,59 +17,84 @@ public class A3Q3 {
      */
     public static void main(String[] args) {
         // create city called kitchener
-        
-        City kitchener = new City (4,7);
-        
-    //create robot named romo
-        
-        Robot romo = new Robot (kitchener,1,1,Direction.EAST);
-        
+
+        City kitchener = new City(4, 7);
+
+        //create robot named romo
+
+        Robot romo = new Robot(kitchener, 1, 1, Direction.EAST);
+
         //creating walls
-        
-        new Wall(kitchener,1,1,Direction.NORTH);
-        new Wall(kitchener,1,2,Direction.NORTH);
-        new Wall(kitchener,1,3,Direction.NORTH);
-        new Wall(kitchener,1,4,Direction.NORTH);
-        new Wall(kitchener,1,1,Direction.WEST);
-        new Wall(kitchener,2,1,Direction.WEST);
-        new Wall(kitchener,3,1,Direction.WEST);
-        new Wall(kitchener,3,1,Direction.SOUTH);
-        new Wall(kitchener,3,2,Direction.SOUTH);
-        new Wall(kitchener,3,3,Direction.SOUTH);
-        new Wall(kitchener,3,4,Direction.SOUTH);
-        new Wall(kitchener,1,4,Direction.EAST);
-        new Wall(kitchener,2,4,Direction.EAST);
-        new Wall(kitchener,3,4,Direction.EAST);
-        
+
+        new Wall(kitchener, 1, 1, Direction.NORTH);
+        new Wall(kitchener, 1, 2, Direction.NORTH);
+        new Wall(kitchener, 1, 3, Direction.NORTH);
+        new Wall(kitchener, 1, 4, Direction.NORTH);
+        new Wall(kitchener, 1, 1, Direction.WEST);
+        new Wall(kitchener, 2, 1, Direction.WEST);
+        new Wall(kitchener, 3, 1, Direction.WEST);
+        new Wall(kitchener, 3, 1, Direction.SOUTH);
+        new Wall(kitchener, 3, 2, Direction.SOUTH);
+        new Wall(kitchener, 3, 3, Direction.SOUTH);
+        new Wall(kitchener, 3, 4, Direction.SOUTH);
+        new Wall(kitchener, 1, 4, Direction.EAST);
+        new Wall(kitchener, 2, 4, Direction.EAST);
+        new Wall(kitchener, 3, 4, Direction.EAST);
+
         //creating things
-        
-         new Thing(kitchener,1,2);
-         new Thing(kitchener,2,2);
-         new Thing(kitchener,2,3);
-         new Thing(kitchener,2,4);
-         new Thing(kitchener,3,1);
-         new Thing(kitchener,3,4);
-         
-         while(romo.frontIsClear()){
-             romo.move();
-             if(romo.canPickThing()){
-                 romo.pickThing();
-             
-             }
-              if(!romo.frontIsClear()){
-              romo.turnLeft();
-              romo.turnLeft();
-              romo.turnLeft();
-              
-          }
-            
-         }
-        
-        
-            
-        
-     
+
+        new Thing(kitchener, 1, 2);
+        new Thing(kitchener, 2, 2);
+        new Thing(kitchener, 2, 3);
+        new Thing(kitchener, 2, 4);
+        new Thing(kitchener, 3, 1);
+        new Thing(kitchener, 3, 4);
+
+        while (romo.frontIsClear()) {
+            romo.move();
+            if (romo.canPickThing()) {
+                romo.pickThing();
+
+            }
+            if (!romo.frontIsClear()) {
+                if (romo.getDirection() == Direction.EAST) {
+                    romo.turnLeft();
+                }
+                romo.turnLeft();
+                romo.turnLeft();
+                if (romo.frontIsClear()) {
+                    romo.move();
+                    romo.turnLeft();
+                    romo.turnLeft();
+                    romo.turnLeft();
+                }
+            } else if (romo.getDirection() == Direction.WEST) {
+                romo.turnLeft();
+                romo.move();
+                romo.turnLeft();
+            }
+
+            if (romo.canPickThing()) {
+                romo.pickThing();
+            }
+        }
+        while (romo.getDirection() != Direction.WEST) {
+
+            while (romo.getAvenue() != 1) {
+
+                while (romo.getDirection() != Direction.NORTH) {
+
+                    while(romo.getStreet() != 1)
+                    {
+                       
+
+                        romo.turnLeft();
+                        romo.turnLeft();
+                        romo.turnLeft();
+
+                    }
+                }
+            }
+        }
     }
 }
-    
-    
